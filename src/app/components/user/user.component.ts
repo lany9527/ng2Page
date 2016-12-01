d0621554950810d6228068d492351db3836dc4dd
lany9527 <lany9527@gmail.com>
2016-12-01 17:30:14 +0800

    添加用户、删除用户功能

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit, Injectable, Input} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  Lists: any;
+  user: any = {};
+
+  constructor(public http: Http) {
+  }
+
+  // 获取用户列表
+  getUsers() {
+    if (this.Lists) {
+      return Promise.resolve(this.Lists);
+    } else {
+      return new Promise(resolve => {
+        this.http.get('http://localhost:8080/api/users')
+          .map(res => res.json())
+          .subscribe(data => {
+            this.Lists = data;
+            console.log(this.Lists);
+            resolve(this.Lists);
+          })
+      })
+    }
+
+  }
+
+  // 添加用户
+  addUser(user) {
+    console.log("add user");
+    let bodyString = JSON.stringify(user);
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+
+    return this.http.post('http://localhost:8080/api/users', bodyString, options)
+      .map((res) => {
+        res.json()
+      })
+      // .catch((error) => { Observable.throw(error.json().error) })
+      .subscribe((user) => {
+        console.log(user)
+      }, (error) => {
+        console.log(error)
+      });
+
+  }
+
+  // 编辑用户信息
+  modifyUser() {
+    console.log("modifyUser");
+  }
+
+  // 删除用户
+  deleteUser(id: string) {
+    console.log('deleteUser');
+    return this.http.delete('http://localhost:8080/api/users/' + `${id}`)
+      .map((res) => {
+        res.json()
+      })
+      .subscribe((user) => {
+        console.log(user), (error) => {
+          console.log(error)
+        }
+      });
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+}
